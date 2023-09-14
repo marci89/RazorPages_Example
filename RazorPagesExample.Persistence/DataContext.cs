4635c853b520b496f1183b3b04c2b37929beccc0
marci <kismarczirobi@gmail.com>
2023-09-14 08:53:32 +0200

    Persistence implementation

---
+++ RazorPagesExample.Persistence/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesExample.Persistence.Entities;
+
+namespace RazorPagesExample.Persistence
+{
+	/// <summary>
+	/// Database context
+	/// </summary>
+	public class DataContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+
+		public DataContext(DbContextOptions options) : base(options) { }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+		}
+	}
+}
--- RazorPagesExample.WEB/Program.cs
+++ RazorPagesExample.WEB/Program.cs
@@ -1,17 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPagesExample.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Home/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
+
+#region If database is not exists or there is a new migration to update, this will handle it.
+
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+if (db.Database.GetPendingMigrations().Any())
+{
+	db.Database.Migrate();
+}
+
+#endregion
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
